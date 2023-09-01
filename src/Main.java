48090097e61edc02861551b296842725df2914f2
ilya03071993 <celiwan@mail.ru>
2023-09-01 13:45:53 +0300

    Add Main

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,6 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, Integer> m1 =new TreeMap<>();
-        Map<String, Integer> m2 = new HashMap<>();
-        put(m1);
-
-    }
-
-    static void put(Map<String, Integer> map) {
-        map.put("Kevin",23);
-        map.put("Luisa",28);
-        map.put("Rodi",13);
+        System.out.println("feature part3");
     }
 }
