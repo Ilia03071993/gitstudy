b974916443e50d8bc56fcd9aac2a2e5efee5a1ce
ilya03071993 <celiwan@mail.ru>
2023-09-01 13:52:57 +0300

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("problem childgit");
+        System.out.println("problem child");
+        int num1 = 100; //1
+        int num2 = 300; //2
+    }
+    public static int result (int num1,int num2) {
+        return num1 + num2; //3
     }
 }
