bb088a883621ae0c639ce38efd78b207425c23ef
ilya03071993 <celiwan@mail.ru>
2023-09-01 15:20:25 +0300

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("problem child");
         int num1 = 100; //1
-   
+        int num2 = 300; //2
+        result(num1 , num2);
     }
-
+    public static int result (int num1, int num2) {
+       return num2 + num1;
+    }
 }
