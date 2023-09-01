d9af80f33a87f022f14e727776c7f91c344755d4
ilya03071993 <celiwan@mail.ru>
2023-09-01 13:36:11 +0300

    Changed Test and Main from GitHub

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         int num1 = 300;
         int num2 = 200;
-        result(num1,num2);
+        result(num1,num2);git 
     }
     public static int result(int num, int num2) {
         return num * num2;
